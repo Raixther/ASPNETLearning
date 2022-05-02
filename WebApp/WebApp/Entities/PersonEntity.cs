f7fedd22edb73f06d52176d2f78ab3d85fee065f
Raixther <81520277+Raixther@users.noreply.github.com>
2022-05-02 19:02:44 +0300

    Add files via upload

---
+++ WebApp/WebApp/Entities/PersonEntity.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebApp.Entities
+{
+[Table("Persons")]
+	public class PersonEntity
+	{
+		[Column("id")]
+		[Key]
+		public int Id { get; set; } 
+
+		[Column("name")]
+		public string Name{ get; set; } = string.Empty;
+
+	}
+}
---
+++ WebApp/WebApp/Mapper/MappingProfile.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+using AutoMapper;
+
+using WebApp.Controllers.DataTransfer;
+using WebApp.Entities;
+
+namespace WebApp.Mapper
+{
+	public class MappingProfile:Profile
+	{
+		public MappingProfile()
+		{
+			CreateMap<PersonEntity, PersonDTO>();
+		}
+
+	}
+}
